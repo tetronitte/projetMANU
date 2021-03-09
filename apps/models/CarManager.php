6d7a5053351f01d9d8351e1676143b82d1022fb1
cmontillot <cedric.montillot@wanadoo.fr>
2021-03-09 11:15:15 +0100

    Update Car Manager

--- apps/models/CarManager.php
+++ apps/models/CarManager.php
@@ -26,4 +26,18 @@ class CarManager extends CI_Models {
         // $this->db->or_where('id',$id);
         // return $this->db->get($this->table);
     }
+
+    public function updatecar(int $id, array $data) {
+		$this->db->set('picture',$data['picture']);
+		$this->db->set('licenceplate',$data['licenceplate']);
+		$this->db->set('detail',$data['detail']);
+		$this->db->set('mileage',$data['mileage']);
+		$this->db->set('disponibility',$data['disponibility']);
+		$this->db->where('id',$id);
+		$this->db->update('cars');
+    }
+
+	public function deletecar(int $id) {
+		$this->db->delete('cars', array('id'=>$id));  
+    }
 }
