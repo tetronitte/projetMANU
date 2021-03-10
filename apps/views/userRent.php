355417eb76a018203b05bbd52b997967de571cfa
WWilliam <myskywan80@gmail.com>
2021-03-10 10:17:55 +0100

    ajout userRent

---
+++ apps/views/userRent.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+	<h3 class="text-center">Véhicules loués :</h3>
+	<div id="listRentUserVehicles" class="row">
+		<div id="hrbar" class="col-12"></div>
+			<?php //foreach(){
+				//avec scroll dans la div et un bouton pour annuler une location
+			//}
+			?>
+	</div>
+
+	<!-- ajouter pagination -->
+</div>
