c79a8afcb6cf1b41a3a7b221414dd4c84bbe6b38
WWilliam <myskywan80@gmail.com>
2021-03-10 13:17:51 +0100

    modification userOption plus plus de paramètre

--- apps/views/userOption.php
+++ apps/views/userOption.php
@@ -2,18 +2,41 @@
 	<div id="listUser" class="row">
 	<?= form_open('url1','class="col-12"'); ?>
 
-		<div class="form-group text-center" id="formLogin">
+		<div class="form-group text-center" id="formUser">
 			<label>Changer d'email :</label>
-				<input type="email" class="form-control" id="emailchange" name="emailchange" value="<?php if(isset($config['emailchange'])){ echo '';};?>">
+				<input type="email" class="form-control" id="mail" name="mailchange" value="<?php if(isset($config['mail'])){ echo '';};?>">
 			<span class="help-block"><?= '';//error ?></span>
 
-			<?= form_submit("send", "Valider",['class' => 'btn btn-warning col-12', 'id' => 'submitUserOption']); ?>
-
-	<?= form_close() ?>
-	<?= form_open('url2','class="col-12"'); ?>
-
 			<label class="col-12">Changer le mot de passe :</label>
-				<input type="text" class="form-control" id="passwordchange" name="passwordchange" value="<?php if(isset($config['passwordchange'])){ echo '';}?>">
+				<input type="text" class="form-control" id="pwd" name="pwd" value="<?php if(isset($config['pwd'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Confirmer le mot de passe :</label>
+				<input type="text" class="form-control" id="pwd" name="pwd" value="<?php if(isset($config['pwd'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre numéro téléphone :</label>
+				<input type="text" class="form-control" id="phone" name="phone" value="<?php if(isset($config['phone'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre Ville :</label>
+				<input type="text" class="form-control" id="city" name="city" value="<?php if(isset($config['city'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre code postal :</label>
+				<input type="text" class="form-control" id="postal" name="postal" value="<?php if(isset($config['postal'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre adresse :</label>
+				<input type="text" class="form-control" id="address" name="address" value="<?php if(isset($config['address'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre numéro de permis de conduire :</label>
+				<input type="text" class="form-control" id="drivingLicence" name="drivingLicence" value="<?php if(isset($config['drivingLicence'])){ echo '';}?>">
+			<span class="help-block"><?= '';//error ?></span>
+
+			<label class="col-12">Changer votre date d'obtention de votre permis de conduire :</label>
+				<input type="date" class="form-control" id="drivingLicenceObtainDate" name="drivingLicenceObtainDate" value="<?php if(isset($config['drivingLicenceObtainDate'])){ echo '';}?>">
 			<span class="help-block"><?= '';//error ?></span>
 
 			<?= form_submit("send", "Valider",['class' => 'btn btn-warning col-12', 'id' => 'submitUserOption']); ?>
