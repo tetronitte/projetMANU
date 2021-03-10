40b1b97e20267612231dbc905dfbb52f69232e83
WWilliam <myskywan80@gmail.com>
2021-03-10 10:47:29 +0100

    modif header pour le css

--- apps/views/templates/header.php
+++ apps/views/templates/header.php
@@ -1,3 +1,4 @@
+<?php 	$css = 'style'; ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
-	<link rel = "stylesheet" type = "text/css" href = "css/style.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel = "stylesheet" type = "text/css" href = "<?= base_url('assets/css/'.$css.'.css') ?>">
   </head>
   <body>
