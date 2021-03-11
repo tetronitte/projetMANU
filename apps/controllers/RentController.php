ebbe0a2e012c64ba5c743781c33ee08b64a3dde2
tetronitte <axel.ndc@gmail.com>
2021-03-11 14:05:51 +0100

    RentManager et RentController

---
+++ apps/controllers/RentController.php
@@ -0,0 +1,35 @@
+<?php
+
+class RentController extends CI_Controller {
+
+    public function list() {
+        $this->load->model('RentManager');
+        $dataContent['title'] = 'Nos locations';
+        $dataContent['css'] = 'style';
+        $rents = $this->RentManager->getAllRents();
+        $dataContent['rents'] = $rents->result();
+        $this->render('listRents',$dataContent);
+    }
+
+    public function add() {
+        $this->load->model('UserManager');
+        $dataContent['title'] = 'Nouvelle location';
+        $dataContent['css'] = 'style';
+        $users = $this->UserManager->getAllUser();
+        $dataContent['users'] = $users->result();
+        $this->render('newRent', $dataContent);
+    }
+
+    public function insert() {
+        $dataContent['title'] = 'Nouvelle location';
+        $dataContent['css'] = 'style';
+        $this->render('newRent', $dataContent);
+    }
+
+    private function render($file, $data) {
+        $this->load->view('templates/header',$data);
+        $this->load->view('templates/navbar',$data);
+        $this->load->view($file,$data);
+        $this->load->view('templates/footer',$data);
+    }
+}
--- apps/models/RentManager.php
+++ apps/models/RentManager.php
@@ -1,9 +1,24 @@
 <?php
 class Rent extends CI_Model {
 
-	public function saveRent(array $data) {
+    private $table;
+
+    public function __construct() {
+        parent::__construct();
+        $this->table = 'rents';
+    }
+
+    public function newRent(array $data) {
         $this->db->insert($this->table,$data);
         return $this->db->insert_id();
+    }
+
+    public function getAllRents() {
+        $this->db->select('rents.id, rents.dateStart, rents.dateEnd, models.name as car, users.lastname as user');
+        $this->db->join('cars', 'cars.id = rents.carsId');
+        $this->db->join('models', 'models.id = cars.modelsId');
+        $this->db->join('users', 'users.id = rents.usersId');
+        return $this->db->get($this->table);
     }
 
 }
