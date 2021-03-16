12f3c77b45b95a2ddb7b9355682f6841f5fa59af
cmontillot <cedric.montillot@wanadoo.fr>
2021-03-16 09:29:33 +0100

    Correction fautes ortho liste véhicules

--- apps/views/listVehicles.php
+++ apps/views/listVehicles.php
@@ -19,8 +19,8 @@
 							<p>Marque : <?= $car->getModel()->getBrand(); ?></p>
 							<p>Type de consommation : <?= $car->getModel()->getFueltype(); ?></p>
 							<p>Catégorie : <?= $car->getModel()->getCategory(); ?></p>
-							<p>nombres de portes : <?= $car->getModel()->getDoors(); ?></p>
-							<h2>Description du vehicule :</h2>
+							<p>Nombres de portes : <?= $car->getModel()->getDoors(); ?></p>
+							<h2>Description du véhicule :</h2>
 							<p><?= $car->getDetails(); ?></p>
 							<h2>Disponibilité :</h2>
 							<?php if($car->getDisponibility() == 1){$Disponibility = 'oui'; } else {$Disponibility = 'non';}?>
